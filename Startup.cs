89f3d3e7815389923e1f9b8f38310eb6e9035065
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2016-07-09 01:16:14 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace HelloAspNetCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
