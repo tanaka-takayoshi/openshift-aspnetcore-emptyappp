6da650fffcf5efa9f613bb5b66e5798a10b41cdd
Takayoshi Tanaka <tanaka-takayoshi@users.noreply.github.com>
2016-07-08 12:24:35 +0900

    Added initial project.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloAspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(url)
+                .Build();
+
+            host.Run();
+        }
+    }
+}
